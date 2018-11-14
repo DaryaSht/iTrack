f84dd4275dac3445f8730deb1d503cc639a9f8b8
Darya Shatrova <darya.shatrov@gmail.com>
2018-11-14 13:13:38 +0300

    first commit

---
+++ src/app/pages/log-in-page/log-in.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'log-in',
+  // template: `<h3>Log In</h3>`
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css'],
+})
+export class LogInComponent { }
---
+++ src/app/pages/registration-page/registration.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
+@Component({
+  selector: 'registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css'],
+})
+export class RegistrationComponent { }
+
+
